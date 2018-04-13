85863420b188629521f1fd3065d2ef443ff6e64f
leonor-loureiro <leonor.filipa@tecnico.ulisboa.pt>
2018-04-13 03:18:07 +0100

    Ping in binas client tested

--- binas-ws-cli/src/test/java/org/binas/ws/it/PingIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/PingIT.java
@@ -1,5 +1,14 @@
 package org.binas.ws.it;
 
+import org.junit.Assert;
+
+import org.binas.ws.EmailExists_Exception;
+import org.binas.ws.InvalidEmail_Exception;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.binas.ws.BadInit_Exception;
+import org.binas.ws.UserView;
 import org.junit.Test;
 
 
@@ -8,14 +17,10 @@ import org.junit.Test;
  */
 public class PingIT extends BaseIT {
 
-    // tests
-    // assertEquals(expected, actual);
-
-    // public String ping(String x)
+   
 
     @Test
     public void pingEmptyTest() {
-		// assertNotNull(binasClient.testPing("test"));
+		assertNotNull(client.testPing("test"));
     }
-
 }
