c7a03003287a4f67ac97514dbbaa15db37d44d8f
Anfifo <afonseca-1995@hotmail.com>
2018-05-01 08:49:37 +0100

    bat that creates all needed terminals for testing windows only

---
+++ windowsTest.bat
@@ -0,0 +1,16 @@
+set "station=%cd%\station-ws"
+set "binasWs=%cd%\binas-ws"
+set "binasCli=%cd%\binas-ws-cli"
+set "stationCli=%cd%\station-ws-cli"
+
+
+start cmd /c "cd %station% & mvn clean install exec:java"
+timeout /t 5
+start cmd /k "cd %station% & mvn exec:java -Dws.i=2"
+start cmd /k "cd %station% & mvn exec:java -Dws.i=3"
+timeout /t 5
+start cmd /c "cd %stationCli% & mvn clean install"
+timeout /t 15
+start cmd /c "cd %binasWs% & mvn clean install exec:java"
+timeout /t 12
+start cmd /k "cd %binasCli% & mvn clean install"
